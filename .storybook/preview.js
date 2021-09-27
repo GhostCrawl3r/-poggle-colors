46752e176a503c66336e0e73eff8ff3dac42041f
Nick Morgan <nick@interstellaragency.co.uk>
2021-09-27 22:29:11 +0100

    First commit.

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+// https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+export const parameters = {
+  // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
+  actions: { argTypesRegex: '^on.*' },
+  options: {
+    storySort: {
+      order: ['Getting Started', 'Styling'],
+    },
+  },
+  previewTabs: {
+    canvas: {
+      hidden: true,
+    },
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// @ts-ignore
+export * from './colours/index.js';
+export * from './intro/intro.components';
+export * from './containers/containers.container';
+export * from './Contributors/contributors.component';
+export * from './typography/typography.components'
+export * from './profile/profile.component'
